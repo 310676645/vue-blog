f38b645651892ba3b4a3cfebfad0b5eb34a2dbdc
songjd <songjinda@rybbaby.com>
2017-03-08 17:33:47 +0800

    优化路由切换动画

--- src/views/home/base.vue
+++ src/views/home/base.vue
@@ -10,18 +10,26 @@
           <el-menu-item index="/home">全部文章</el-menu-item>
         </el-menu>
       </el-col>
-      <transition
-        enter-active-class="animated bounceInLeft"
-        leave-active-class="animated bounceOutRight" mode="out-in">
-        <router-view
-          class="md-main">
-        </router-view>
-      </transition>
+      <el-col :span="24">
+        <div class="md-container">
+          <transition
+            enter-active-class="animated bounceInLeft"
+            leave-active-class="animated bounceOutRight">
+              <router-view
+                class="md-main">
+              </router-view>
+          </transition>
+        </div>
+      </el-col>
     </el-row>
 </template>
 <style lang="scss" rel="stylesheet/scss" type="text/css" scoped>
   @import "./../../assets/sass/modules/var";
+  .#{$baseName}-container {
+    position: relative;
+  }
   .#{$baseName}-main {
+    position: absolute;
     padding: $gutter;
     box-sizing: border-box;
     width: 100%;
